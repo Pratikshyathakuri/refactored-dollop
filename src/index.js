4097c97cd04b9af2eacffad99092dfa007242980
aayush6194 <thakuripratikshya1@gmail.com>
2019-09-21 21:17:33 -0500

    created an awesome project

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+
+
+app.get('/', function(req, res){
+    console.log('A Request');
+    res.sendFile(__dirname + '/html/index.html');
+})
+
+app.get('/home', function(req, res){
+    console.log('A Request');
+    res.sendFile(__dirname + '/html/index.html');
+})
+
+app.get('/about', function(req, res){
+    console.log('A Request');
+    res.sendFile(__dirname + '/html/about.html');
+})
+
+app.get('/submit', function(req, res){
+    let name = req.query.name;
+    let phone = req.query.phone;
+    let text = 'Name : ' + name + '\nPhone : ' + phone;
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: { user :  'finapp.lsbdc@gmail.com', pass: 'racoons@1'}
+       });
+     transporter.sendMail({
+         from: 'finapp.lsbdc@gmail.com',
+         to : 'thakuripratikshya1@gmail.com',
+         subject: 'User\'s Phone Number',
+         text : text
+     });
+
+     transporter.sendMail({
+        from: 'finapp.lsbdc@gmail.com',
+        to : 'thakurps@warhawks.ulm.edu',
+        subject: 'User\'s Phone Number',
+        text : text
+    });
+    res.redirect("/")
+})
+
+
+app.get('*', function(req, res){
+    console.log('A Request');
+    res.sendFile(__dirname + '/html/404.html');
+})
+
+
+app.listen(80);
+
+console.log("Server is Running on Port 80");
